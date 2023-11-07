20c31d1cd36441319ce0472ca71a3b527d83fd4b
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-11-07 06:20:45 -0300

    REFACTIR: Update server settings

--- src/env/index.ts
+++ src/env/index.ts
@@ -13,7 +13,7 @@ const envSchema = z.object({
     NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
     DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
     DATABASE_URL: z.string(),
-    PORT: z.coerce.number(), // o coerce serve para converter uma variável, nesse caso em um número
+    PORT: z.coerce.number().default(3333), // o coerce serve para converter uma variável, nesse caso em um número
 });
 
 const _env = envSchema.safeParse(process.env);
